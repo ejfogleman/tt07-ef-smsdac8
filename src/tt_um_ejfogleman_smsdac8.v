a4af3fe5dd69a141248eefd251d942a77cedc135
ejfogleman <fogleman@ieee.org>
2024-05-12 21:48:26 -0400

    Fix top module name

--- src/tt_um_ejfogleman_smsdac8.v
+++ src/tt_um_ejfogleman_smsdac8.v
@@ -13,7 +13,7 @@
 `default_nettype none
 `timescale 1ns / 100ps
 
-module tt_um_ejfogleman_smsdac (
+module tt_um_ejfogleman_smsdac8 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
